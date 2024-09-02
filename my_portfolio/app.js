97788fa3be9c1878af2c71d8fb710680688ae779
Ali <alouloucheikh05@gmail.com>
2024-09-02 07:15:13 +0100

    "Added contact form functionality with Google Sheets integration, updated HTML and CSS for form styling, and included necessary JavaScript libraries"

--- my_portfolio/app.js
+++ my_portfolio/app.js
@@ -11,3 +11,83 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+const scriptURL = 'Your url from deployment';
+
+$(document).ready(function () {
+    $('#contact-form').on('submit', function (e) {
+        e.preventDefault();
+
+        Swal.fire({
+            icon: 'info',
+            title: 'Please wait...',
+            text: 'Sending your message',
+            willOpen: () => {
+                Swal.showLoading();
+            },
+            showConfirmButton: false,
+            allowOutsideClick: false,
+            background: "#00ACA8"
+        });
+
+        const formData = new FormData(this);
+
+        $.ajax({
+            url: scriptURL,
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                console.log("Response from Google Sheets:", data);
+
+                if (data.result === 'success') {
+                    Swal.fire({
+                        imageUrl: "assets/img/Lingoland_logo.png",
+                        imageAlt: "Custom Success Icon",
+                        title: 'Success!',
+                        text: 'Your message is sent. Thank you for reaching out.',
+                        width: 600,
+                        padding: "3em",
+                        timer: 5000,
+                        timerProgressBar: true,
+                        showConfirmButton: false,
+                        allowOutsideClick: false,
+                        color: "#000",
+                        background: "#00ACA8",
+                        backdrop: "rgba(0,0,0,0.4)",
+                    }).then(() => {
+                        window.location.reload();
+                    });
+                } else {
+                    console.error('Error:', data.error);
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong! Please try again later.',
+                    });
+                }
+            },
+            error: function (error) {
+                console.error('Error!', error);
+                Swal.fire({
+                        imageUrl: "assets/img/Lingoland_logo.png",
+                        imageAlt: "Custom Success Icon",
+                        title: 'Success!',
+                        text: 'Your message is sent. Thank you for reaching out.',
+                        width: 600,
+                        padding: "3em",
+                        timer: 5000,
+                        timerProgressBar: true,
+                        showConfirmButton: false,
+                        allowOutsideClick: false,
+                        color: "#000",
+                        background: "#00ACA8",
+                        backdrop: "rgba(0,0,0,0.4)",
+                    }).then(() => {
+                        window.location.reload();
+                    });
+            }
+        });
+    });
+});
